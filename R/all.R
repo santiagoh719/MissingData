c65f4b968363769f1d166367ba60434d028399d3
santiagoh719 <43649362+santiagoh719@users.noreply.github.com>
2019-09-30 14:12:26 -0300

    Update all.R

--- R/all.R
+++ R/all.R
@@ -3,24 +3,24 @@ interpolate_all <- function(Est_target,CF = FALSE,CF_fun='median',fun,...){
   
   my_fun <- possibly(.f = fun,otherwise = NA)
   na <- is.na(Est_target)
-  
-  CC <- 0
-  if(CF){
-    new_target <- rep(NA,length(Est_target))
-    na_index <- which(!na)
-    for(i in na_index){
-      new_target[i] <- my_fun(Est_target = Est_target,i=i,...) - Est_target
-    }
-    if(CF_fun == 'median'){
-      CC <- median(new_target,na.rm = T)
-    }else if(CF_fun =='mean'){
-      CC <- mean(new_target,na.rm = T)
-    }else{
-      stop('not suported CF_fun, only support median or mean')
-    }
-  }
-  
   if(any(na)){
+    CC <- 0
+    if(CF){
+      new_target <- rep(NA,length(Est_target))
+      na_index <- which(!na)
+      for(i in na_index){
+        new_target[i] <- my_fun(Est_target = Est_target,i=i,...) - Est_target[i]
+      }
+      if(CF_fun == 'median'){
+        CC <- median(new_target,na.rm = T)
+      }else if(CF_fun =='mean'){
+        CC <- mean(new_target,na.rm = T)
+      }else{
+        stop('not suported CF_fun, only support median or mean')
+      }
+    }
+  
+  
     new_target <- Est_target
     na_index <- which(na)
     for(i in na_index){
